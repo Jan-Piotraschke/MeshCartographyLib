25296bff9c169d4feff282640280dfae04535140
Jan-Piotraschke <piotraschke.janniklas@gmail.com>
2023-09-13 09:20:35 +0200

    unittesting whether 2D point is inside polygon

--- tests/test_SurfaceParametrization.cpp
+++ tests/test_SurfaceParametrization.cpp
@@ -15,9 +15,11 @@
 #include <fstream>
 #include <boost/filesystem.hpp>
 #include <memory>
+#include <random>
 
 #include <SurfaceParametrization.h>
 
+bool original_mesh = true;
 bool free_boundary = false;
 SurfaceParametrization surface_parametrization(free_boundary);
 const boost::filesystem::path PROJECT_PATH = MeshCartographyLib_SOURCE_DIR;
@@ -135,6 +137,72 @@ TEST_F(SurfaceParametrizationTest, MaxDistance) {
     EXPECT_EQ(max_distance, expected_distance);
 }
 
+TEST_F(SurfaceParametrizationTest, CheckPointInPolygon) {
+    // 1. Points clearly inside the square
+    std::vector<Point_2_eigen> inside_points = {
+        {0.5, 0.5},
+        {0.25, 0.25},
+        {0.75, 0.75},
+        {0.4, 0.6}
+    };
+    for (const auto& point : inside_points) {
+        EXPECT_TRUE(surface_parametrization.check_point_in_polygon(point, original_mesh));
+    }
+}
+
+TEST_F(SurfaceParametrizationTest, CheckPointOutsidePolygon) {
+    // 2. Points clearly outside the square
+    std::vector<Point_2_eigen> outside_points = {
+        {-0.1, 0.5},
+        {1.1, 0.5},
+        {0.5, -0.1},
+        {0.5, 1.1}
+    };
+    for (const auto& point : outside_points) {
+        EXPECT_FALSE(surface_parametrization.check_point_in_polygon(point, original_mesh));
+    }
+}
+
+TEST_F(SurfaceParametrizationTest, CheckPointOnBoundary) {
+    // 3. Points right on the boundary
+    std::vector<Point_2_eigen> boundary_points = {
+        {0, 0},
+        {1, 0},
+        {0, 1},
+        {1, 1},
+        {0.5, 0},
+        {0.5, 1},
+        {0, 0.5},
+        {1, 0.5}
+    };
+    for (const auto& point : boundary_points) {
+        EXPECT_TRUE(surface_parametrization.check_point_in_polygon(point, original_mesh));
+    }
+}
+
+TEST_F(SurfaceParametrizationTest, CheckPointInPolygonGrid) {
+    // 4. Grid of points covering the region
+    double step = 0.01; // you can adjust this as needed
+    for (double x = -0.2; x <= 1.2; x += step) {
+        for (double y = -0.2; y <= 1.2; y += step) {
+            bool is_inside = (x >= 0 && x <= 1 && y >= 0 && y <= 1);
+            EXPECT_EQ(surface_parametrization.check_point_in_polygon({x, y}, original_mesh), is_inside);
+        }
+    }
+}
+
+TEST_F(SurfaceParametrizationTest, CheckPointInPolygonRandom) {
+    // 5. Randomly generated points
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> dis(-0.2, 1.2);  // random numbers between -0.2 to 1.2
+    for (int i = 0; i < 1000; ++i) {
+        double x = dis(gen);
+        double y = dis(gen);
+        bool is_inside = (x >= 0 && x <= 1 && y >= 0 && y <= 1);
+        EXPECT_EQ(surface_parametrization.check_point_in_polygon({x, y}, original_mesh), is_inside);
+    }
+}
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
