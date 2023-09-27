39e38a44f76b8b22cb87ef62a69ca10182b2dc97
Jan-Piotraschke <piotraschke.janniklas@gmail.com>
2023-09-27 12:01:14 +0200

    square border helper script

--- src/SurfaceParametrization/SurfaceParametrization.cpp
+++ src/SurfaceParametrization/SurfaceParametrization.cpp
@@ -14,6 +14,7 @@
 
 #include "CutLineHelper.h"
 #include "SquareBorderParametrizationHelper.h"
+#include "SquareBorderHelper.h"
 
 SurfaceParametrization::SurfaceParametrization(){}
 
@@ -84,65 +85,6 @@ bool SurfaceParametrization::has_boundary(const pmp::SurfaceMesh& mesh)
     return false;
 }
 
-void SurfaceParametrization::setup_boundary_constraints(pmp::SurfaceMesh& mesh)
-{
-    // get properties
-    auto points = mesh.vertex_property<pmp::Point>("v:point");
-    auto tex = mesh.vertex_property<pmp::TexCoord>("v:tex");
-
-    pmp::SurfaceMesh::VertexIterator vit, vend = mesh.vertices_end();
-    pmp::Vertex vh;
-    pmp::Halfedge hh;
-    std::vector<pmp::Vertex> loop;
-
-    // Initialize all texture coordinates to the origin.
-    for (auto v : mesh.vertices()) {
-        tex[v] = pmp::TexCoord(0.5, 0.5);
-    }
-
-    // find 1st boundary vertex
-    for (vit = mesh.vertices_begin(); vit != vend; ++vit) {
-        if (mesh.is_boundary(*vit)) {
-            break;
-        }
-    }
-
-    // collect boundary loop
-    vh = *vit;
-    hh = mesh.halfedge(vh);
-    do
-    {
-        loop.push_back(mesh.to_vertex(hh));
-        hh = mesh.next_halfedge(hh);
-    } while (hh != mesh.halfedge(vh));
-
-    // map boundary loop to unit circle in texture domain
-    unsigned int i, n = loop.size();
-    std::cout << "Boundary loop: " << n << " vertices" << std::endl;
-    pmp::Scalar angle, l, length;
-    pmp::TexCoord t;
-
-    // compute length of boundary loop
-    for (i = 0, length = 0.0; i < n; ++i) {
-        length += distance(points[loop[i]], points[loop[(i + 1) % n]]);
-    }
-
-    // map length intervalls to unit circle intervals
-    for (i = 0, l = 0.0; i < n;) {
-        // go from 2pi to 0 to preserve orientation
-        angle = 2.0 * M_PI * (1.0 - l / length);
-
-        t[0] = 0.5 + 0.5 * cosf(angle);
-        t[1] = 0.5 + 0.5 * sinf(angle);
-
-        tex[loop[i]] = t;
-
-        ++i;
-        if (i < n) {
-            l += distance(points[loop[i]], points[loop[(i + 1) % n]]);
-        }
-    }
-}
 
 void SurfaceParametrization::harmonic_parameterization(pmp::SurfaceMesh& mesh, bool use_uniform_weights)
 {
@@ -153,8 +95,9 @@ void SurfaceParametrization::harmonic_parameterization(pmp::SurfaceMesh& mesh, b
         throw pmp::InvalidInputException(what);
     }
 
-    // map boundary to circle
-    setup_boundary_constraints(mesh);
+    // create boundary
+    SquareBorderHelper border_helper = SquareBorderHelper(mesh);
+    border_helper.setup_square_boundary_constraints();
 
     // get properties
     auto UV_coord = mesh.vertex_property<pmp::TexCoord>("v:tex");
@@ -186,11 +129,6 @@ void SurfaceParametrization::harmonic_parameterization(pmp::SurfaceMesh& mesh, b
     for (auto v : mesh.vertices()) {
         if (!mesh.is_boundary(v))
             UV_coord[v] = X.row(v.idx());
-    }
-
-    // print UV_coord coords
-    for (auto v : mesh.vertices()) {
-        std::cout << "v[" << v << "] = " << UV_coord[v] << std::endl;
     }
 }
 
