9837989a522f5bd9bb571013b0609ac638bab2a0
Jan-Piotraschke <piotraschke.janniklas@gmail.com>
2023-10-23 13:51:21 +0200

    updated unittest to the new code structure

--- tests/test_GeodesicDistance.cpp
+++ tests/test_GeodesicDistance.cpp
@@ -1,39 +1,53 @@
-// /**
-//  * @file        test_GeodesicDistance.cpp
-//  * @brief       Testings for the GeodesicDistance class
-//  *
-//  * @author      Jan-Piotraschke
-//  * @date        2023-Sep-08
-//  * @license     Apache License 2.0
-//  *
-//  * @bug         -
-//  * @todo        -
-//  */
+/**
+ * @file        test_GeodesicDistance.cpp
+ * @brief       Testings for the GeodesicDistance class
+ *
+ * @author      Jan-Piotraschke
+ * @date        2023-Sep-08
+ * @license     Apache License 2.0
+ *
+ * @bug         -
+ * @todo        -
+ */
 
-// #include <gtest/gtest.h>
-// #include <fstream>
-// #include <boost/filesystem.hpp>
-// #include <memory>
-// namespace fs = boost::filesystem;
+#include <gtest/gtest.h>
+#include <fstream>
+#include <filesystem>
+#include <memory>
+namespace fs = std::filesystem;
 
-// #include "GeodesicDistance/CachedGeodesicDistanceHelper.h"
+#include "SurfaceParametrization/TessellationHelper.h"
+#include "SurfaceParametrization/SurfaceParametrization.h"
 
-// fs::path MESH_CARTOGRAPHY = MeshCartographyLib_SOURCE_DIR;
-// fs::path mesh_file_ellipsoid = MESH_CARTOGRAPHY / "meshes/sphere.off";
-// CachedGeodesicDistanceHelper geodesic_distance_helper(mesh_file_ellipsoid);
+#include "GeodesicDistance/CachedGeodesicDistanceHelper.h"
+#include "GeodesicDistance/CachedTessellationDistanceHelper.h"
 
-// class GeodesicDistanceTest : public ::testing::Test {
-// protected:
-//     void SetUp() override {
-//     }
-// };
+fs::path MESH_CARTOGRAPHY = MeshCartographyLib_SOURCE_DIR;
+fs::path mesh = MESH_CARTOGRAPHY / "meshes/sphere.off";
+fs::path mesh_open = MESH_CARTOGRAPHY / "meshes/sphere_open.off";
+fs::path mesh_kachelmuster = MESH_CARTOGRAPHY / "meshes/sphere_uv_kachelmuster.off";
 
-// TEST_F(GeodesicDistanceTest, GetAllDistances) {
-//     ASSERT_NO_THROW(geodesic_distance_helper.get_mesh_distance_matrix());
-// }
+SurfaceParametrization _surface_parametrization = SurfaceParametrization();
+Tessellation tessellation(_surface_parametrization);
 
-// TEST_F(GeodesicDistanceTest, DistanceMatrixSaved) {
-//     fs::path distance_matrix_path = MESH_CARTOGRAPHY / "meshes/data/sphere_distance_matrix_static.csv";
-//     std::ifstream infile(distance_matrix_path.string());
-//     ASSERT_TRUE(infile.good());
-// }
+class GeodesicDistanceTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        _surface_parametrization.create_uv_surface(mesh, 0);
+        std::vector<std::vector<int64_t>> equivalent_vertices = tessellation.create_kachelmuster();
+
+        CachedTessellationDistanceHelper helper = CachedTessellationDistanceHelper(mesh_kachelmuster, equivalent_vertices);
+        GeodesicDistanceHelperInterface& geodesic_distance_helper = helper;
+        auto distance_matrix_tessellation = geodesic_distance_helper.get_mesh_distance_matrix();
+
+        CachedGeodesicDistanceHelper helper_3D = CachedGeodesicDistanceHelper(mesh_open);
+        GeodesicDistanceHelperInterface& geodesic_distance_helper_3D = helper_3D;
+        auto distance_matrix_3D = geodesic_distance_helper_3D.get_mesh_distance_matrix();
+    }
+};
+
+TEST_F(GeodesicDistanceTest, DistanceMatrixSaved) {
+    fs::path distance_matrix_path = MESH_CARTOGRAPHY / "meshes/data/sphere_open_distance_matrix_static.csv";
+    std::ifstream infile(distance_matrix_path.string());
+    ASSERT_TRUE(infile.good());
+}
