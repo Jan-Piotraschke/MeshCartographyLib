b9391868f228197990e70e35f18c03b438017529
Jan-Piotraschke <piotraschke.janniklas@gmail.com>
2023-10-23 11:25:59 +0200

    dijkstra algorithmus

---
+++ src/GeodesicDistance/DijkstraDistanceHelper.cpp
@@ -0,0 +1,78 @@
+/**
+ * @file        DijkstraDistanceHelper.cpp
+ * @brief       Zuständing für die Berechnung basierend auf den Dijkstra-Algorithmus
+ *
+ * @author      Jan-Piotraschke
+ * @date        2023-Oct-23
+ * @license     Apache License 2.0
+ *
+ * @bug         -
+ * @todo        -
+ */
+
+#include "DijkstraDistanceHelper.h"
+
+DijkstraDistanceHelper::DijkstraDistanceHelper(fs::path mesh_path) : mesh_path(mesh_path) {}
+
+
+// ========================================
+// Public Functions
+// ========================================
+
+/**
+ * @brief Calculate the distance using the Heat Method
+*/
+Eigen::MatrixXd DijkstraDistanceHelper::get_mesh_distance_matrix() {
+    pmp::SurfaceMesh mesh;
+    pmp::read_off(mesh, mesh_path.string());
+
+    size_t numVerts = mesh.n_vertices();
+    Eigen::MatrixXd distance_matrix_v(numVerts, numVerts);
+
+    // loop over all vertices and fill the distance matrix
+    for (auto vi : mesh.vertices()) {
+        fill_distance_matrix(mesh, distance_matrix_v, vi);
+    }
+
+    return distance_matrix_v;
+}
+
+
+
+// ========================================
+// Private Functions
+// ========================================
+
+/**
+ * @brief Variable to keep track of the current index of the vector of distances, and each thread processes a
+ * different index until all the distances have been added to the distance matrix.
+*/
+void DijkstraDistanceHelper::fill_distance_matrix(
+    pmp::SurfaceMesh& mesh,
+    Eigen::MatrixXd& distance_matrix,
+    pmp::Vertex vertex
+){
+    if (distance_matrix.row(vertex.idx()).head(2).isZero()) {
+        // get the distance of all vertices to all other vertices
+        std::vector<double> vertices_3D_distance_map = calculate_geodesic_distance(mesh, vertex);
+        distance_matrix.row(vertex.idx()) = Eigen::Map<Eigen::VectorXd>(vertices_3D_distance_map.data(), vertices_3D_distance_map.size());
+    }
+}
+
+
+std::vector<double> DijkstraDistanceHelper::calculate_geodesic_distance(
+    pmp::SurfaceMesh& mesh,
+    pmp::Vertex start_vertex
+){
+    std::vector<pmp::Vertex> seeds{start_vertex};
+    pmp::geodesics(mesh, seeds);
+
+    pmp::VertexProperty<pmp::Scalar> distance_pmap = mesh.get_vertex_property<pmp::Scalar>("geodesic:distance");
+
+    std::vector<double> distances;
+    for (pmp::Vertex vertex : mesh.vertices()) {
+        distances.push_back(distance_pmap[vertex]);
+    }
+
+    return distances;
+}
---
+++ src/GeodesicDistance/DijkstraDistanceHelper.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <Eigen/Dense>
+
+#include "MeshDefinition.h"
+#include "GeodesicDistanceHelperInterface.h"
+
+class DijkstraDistanceHelper : public GeodesicDistanceHelperInterface {
+public:
+    DijkstraDistanceHelper(fs::path mesh_path);
+
+    Eigen::MatrixXd get_mesh_distance_matrix() override;
+
+private:
+    fs::path mesh_path;
+
+    void fill_distance_matrix(
+        pmp::SurfaceMesh& mesh,
+        Eigen::MatrixXd& distance_matrix,
+        pmp::Vertex vertex
+    );
+
+    std::vector<double> calculate_geodesic_distance(
+        pmp::SurfaceMesh& mesh,
+        pmp::Vertex start_vertex
+    );
+};
