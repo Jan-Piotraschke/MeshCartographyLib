ade8ec9716a51e9082e8e21b0a0459a01d03fc9b
Jan-Piotraschke <piotraschke.janniklas@gmail.com>
2023-10-23 17:14:03 +0200

    slicing the tessellation distance mesh for comparision

--- tests/test_GeodesicDistance.cpp
+++ tests/test_GeodesicDistance.cpp
@@ -7,7 +7,7 @@
  * @license     Apache License 2.0
  *
  * @bug         -
- * @todo        - fix the comparison between the 3D and the tessellation distance matrix, because the UV mesh has more vertices than the 3D mesh
+ * @todo        -
  */
 
 #include <gtest/gtest.h>
@@ -23,9 +23,9 @@ namespace fs = std::filesystem;
 #include "GeodesicDistance/CachedTessellationDistanceHelper.h"
 
 fs::path MESH_CARTOGRAPHY = MeshCartographyLib_SOURCE_DIR;
-fs::path mesh = MESH_CARTOGRAPHY / "meshes/sphere.off";
-fs::path mesh_open = MESH_CARTOGRAPHY / "meshes/sphere_open.off";
-fs::path mesh_kachelmuster = MESH_CARTOGRAPHY / "meshes/sphere_uv_kachelmuster.off";
+fs::path mesh = MESH_CARTOGRAPHY / "meshes/ellipsoid_x4.off";
+fs::path mesh_open = MESH_CARTOGRAPHY / "meshes/ellipsoid_x4_open.off";
+fs::path mesh_kachelmuster = MESH_CARTOGRAPHY / "meshes/ellipsoid_x4_uv_kachelmuster.off";
 
 SurfaceParametrization _surface_parametrization = SurfaceParametrization();
 Tessellation tessellation(_surface_parametrization);
@@ -37,11 +37,18 @@ protected:
 
     void SetUp() override {
         _surface_parametrization.create_uv_surface(mesh, 0);
+        // Original Ellipsoid mesh has 4670 vertices
+        int n_mesh = 4670;
+
         std::vector<std::vector<int64_t>> equivalent_vertices = tessellation.create_kachelmuster();
 
         CachedTessellationDistanceHelper helper = CachedTessellationDistanceHelper(mesh_kachelmuster, equivalent_vertices);
         GeodesicDistanceHelperInterface& geodesic_distance_helper = helper;
         distance_matrix_tessellation = geodesic_distance_helper.get_mesh_distance_matrix();
+
+        // Slice the matrix to only contain the original vertices
+        Eigen::MatrixXd slicedMatrix = distance_matrix_tessellation.block(0, 0, n_mesh, n_mesh);
+        distance_matrix_tessellation = slicedMatrix;
 
         CachedGeodesicDistanceHelper helper_3D = CachedGeodesicDistanceHelper(mesh);
         GeodesicDistanceHelperInterface& geodesic_distance_helper_3D = helper_3D;
@@ -63,7 +70,7 @@ protected:
 };
 
 TEST_F(GeodesicDistanceTest, DistanceMatrixSaved) {
-    fs::path distance_matrix_path = MESH_CARTOGRAPHY / "meshes/data/sphere_open_distance_matrix_static.csv";
+    fs::path distance_matrix_path = MESH_CARTOGRAPHY / "meshes/data/ellipsoid_x4_distance_matrix_static.csv";
     std::ifstream infile(distance_matrix_path.string());
     ASSERT_TRUE(infile.good());
 }
