0e7e0397c10fba09c0c394a8f5ba6f349b324b25
Jan-Piotraschke <piotraschke.janniklas@gmail.com>
2023-10-26 12:09:17 +0200

    code refactoring

--- MeshCartography.cpp
+++ MeshCartography.cpp
@@ -23,20 +23,10 @@ T Clamp(const T& value, const T& low, const T& high) {
     return std::max(low, std::min(value, high));
 }
 
-
-struct MonotileAreaCostFunction {
-    MonotileAreaCostFunction(double a, double b) : a_(a), b_(b) {}
-
+class AreaCalculator {
+public:
     template <typename T>
-    bool operator()(const T* const curve_strength, T* residual) const {
-        // Make sure curve_strength is within the boundaries
-        T cs = Clamp(*curve_strength, T(0), T(3));
-
-        // You can use your existing draw_monotile function here to generate the border
-        std::vector<T> x_vals, y_vals;
-        draw_monotile(T(a_), T(b_), cs, x_vals, y_vals);
-
-        // Calculate the area using the Shoelace formula
+    T ComputeArea(const std::vector<T>& x_vals, const std::vector<T>& y_vals) const {
         T area = T(0);
         int n = x_vals.size();
         for (int i = 0; i < n - 1; ++i) {
@@ -45,7 +35,27 @@ struct MonotileAreaCostFunction {
         }
         area += x_vals[n - 1] * y_vals[0];
         area -= y_vals[n - 1] * x_vals[0];
-        area = ceres::abs(area) / T(2);
+        return ceres::abs(area) / T(2);
+    }
+};
+
+
+struct MonotileAreaCostFunction {
+    MonotileAreaCostFunction(double a, double b) : a_(a), b_(b) {}
+
+    double final_area = 0.0;
+    AreaCalculator area_calculator;
+
+    template <typename T>
+    bool operator()(const T* const curve_strength, T* residual) const {
+        // Make sure curve_strength is within the boundaries
+        T cs = Clamp(*curve_strength, T(0), T(3));
+
+        std::vector<T> x_vals, y_vals;
+        draw_monotile(T(a_), T(b_), cs, x_vals, y_vals);
+
+        // Calculate the area using the Shoelace formula
+        T area = area_calculator.ComputeArea(x_vals, y_vals);
 
         // Since Ceres performs minimization, we return the negative area to maximize it
         residual[0] = -area;
@@ -56,81 +66,52 @@ struct MonotileAreaCostFunction {
 };
 
 
+class OptimizationProblem {
+public:
+    void Run(double a, double b, double& curve_strength) {
+        ceres::Problem problem;
+
+        MonotileAreaCostFunction* cost_function = new MonotileAreaCostFunction(a, b);
+        problem.AddResidualBlock(
+            new ceres::AutoDiffCostFunction<MonotileAreaCostFunction, 1, 1>(cost_function),
+            nullptr, &curve_strength);
+
+        problem.SetParameterLowerBound(&curve_strength, 0, lower_bound_);
+        problem.SetParameterUpperBound(&curve_strength, 0, upper_bound_);
+
+        ceres::Solver::Options options;
+        options.minimizer_progress_to_stdout = true;
+        options.linear_solver_type = ceres::DENSE_QR;
+
+        ceres::Solver::Summary summary;
+        ceres::Solve(options, &problem, &summary);
+
+        std::cout << summary.FullReport() << "\n";
+        std::cout << "Final curve_strength: " << curve_strength << "\n";
+    }
+
+    void SetBounds(double lower_bound, double upper_bound) {
+        lower_bound_ = lower_bound;
+        upper_bound_ = upper_bound;
+    }
+
+private:
+    double lower_bound_ = -18.0;
+    double upper_bound_ = 3.0;
+};
+
+
 int main() {
     double a = 1.0;
     double b = 1.0;
     double curve_strength = 1.0; // Initial guess
 
-    ceres::Problem problem;
-
-    problem.AddResidualBlock(
-        new ceres::AutoDiffCostFunction<MonotileAreaCostFunction, 1, 1>(
-            new MonotileAreaCostFunction(a, b)),
-        nullptr, &curve_strength);
-
-    ceres::Solver::Options options;
-    options.minimizer_progress_to_stdout = true;
-    options.linear_solver_type = ceres::DENSE_QR;
-
-    ceres::Solver::Summary summary;
-    ceres::Solve(options, &problem, &summary);
-
-    std::cout << summary.FullReport() << "\n";
-    std::cout << "Final curve_strength: " << curve_strength << "\n";
+    OptimizationProblem optimization_problem;
+    optimization_problem.SetBounds(-18.0, 3.0);
+    optimization_problem.Run(a, b, curve_strength);
 
     return 0;
 }
-
-
-// struct ControlPointsFunctor {
-//     template <typename T>
-//     bool operator()(const T* const point, T* residuals) const {
-//         T x = point[0];
-//         T y = point[1];
-//         T curve_strength = T(1.0); // Assuming some curve strength for this example
-
-//         T normal_x = y;
-//         T normal_y = -x;
-
-//         // Control point 1
-//         residuals[0] = -curve_strength * normal_x + x / T(2.0);
-//         residuals[1] = -curve_strength * normal_y + y / T(2.0);
-
-//         // Control point 2
-//         residuals[2] = curve_strength * normal_x + x / T(2.0);
-//         residuals[3] = curve_strength * normal_y + y / T(2.0);
-
-//         return true;
-//     }
-// };
-
-// int main() {
-//     double point[2] = {1.0, 2.0};
-
-//     // Build the problem.
-//     ceres::Problem problem;
-
-//     // Set up the cost function (also known as residual). This will be minimized using
-//     // the Levenberg-Marquardt method.
-//     ceres::CostFunction* cost_function =
-//         new ceres::AutoDiffCostFunction<ControlPointsFunctor, 4, 2>(new ControlPointsFunctor);
-
-//     problem.AddResidualBlock(cost_function, nullptr, point);
-
-//     // Run the solver!
-//     ceres::Solver::Options options;
-//     options.linear_solver_type = ceres::DENSE_QR;
-//     options.minimizer_progress_to_stdout = true;
-
-//     ceres::Solver::Summary summary;
-//     Solve(options, &problem, &summary);
-
-//     std::cout << summary.BriefReport() << "\n";
-//     std::cout << "Original point: (1, 2)\n";
-//     std::cout << "Optimized point: (" << point[0] << ", " << point[1] << ")\n";
-
-//     return 0;
-// }
 
 
 
