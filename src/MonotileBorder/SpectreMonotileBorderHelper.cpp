4ad086b48baa47018dfa7861346bfa7ffcc9a240
Jan-Piotraschke <piotraschke.janniklas@gmail.com>
2023-11-02 07:37:04 +0100

    recalculating the corner points

--- src/MonotileBorder/SpectreMonotileBorderHelper.cpp
+++ src/MonotileBorder/SpectreMonotileBorderHelper.cpp
@@ -35,23 +35,56 @@ void SpectreMonotileBorderHelper::setup_spectre_monotile_boundary_constraints(do
     std::vector<double> x_vals, y_vals;
     spectre_border(a, b, curve_strength, x_vals, y_vals);
 
-    // Map the calculated border to texture coordinates
-    mapToSpectreMonotile(x_vals, y_vals);
+    // // Compute the total length of the border
+    // double totalLength = 0.0;
+    // for (size_t i = 0; i < x_vals.size() - 1; ++i) {
+    //     double dx = x_vals[i+1] - x_vals[i];
+    //     double dy = y_vals[i+1] - y_vals[i];
+    //     totalLength += std::sqrt(dx * dx + dy * dy);
+    // }
+
+    // // Assuming the spectre has 14 edges
+    // double sideLength = totalLength / 14.0;
+
+    // Initialize corners
+    initializeCorners(a, b, curve_strength);
 }
 
-void SpectreMonotileBorderHelper::mapToSpectreMonotile(const std::vector<double>& x_vals, const std::vector<double>& y_vals)
-{
-    auto tex = mesh.vertex_property<pmp::TexCoord>("v:tex");
 
-    pmp::Halfedge hh = mesh.halfedge(start_vertex);
-    size_t index = 0;
+void SpectreMonotileBorderHelper::initializeCorners(double a, double b, double curve_strength) {
+    corners.clear();
 
-    do {
-        if (index < x_vals.size()) {
-            pmp::Vertex v = mesh.from_vertex(hh);
-            tex[v] = pmp::TexCoord(x_vals[index], y_vals[index]);
-            index++;
-        }
-        hh = mesh.next_halfedge(hh);
-    } while (hh != mesh.halfedge(start_vertex) && index < x_vals.size());
+    // Define the direction vectors for your spectre shape
+    double cos_angle = ceres::cos(M_PI / 3.0);
+    double sin_angle = ceres::sin(M_PI / 3.0);
+    std::vector<std::pair<double, double>> direction_vectors = {
+        {cos_angle * b, sin_angle * b},
+        {b, 0},
+        {0, a},
+        {sin_angle * a, cos_angle * a},
+        {cos_angle * b, -sin_angle * b},
+        {-cos_angle * b, -sin_angle * b},
+        {sin_angle * a, -cos_angle * a},
+        {0, -a},
+        {0, -a},
+        {-sin_angle * a, -cos_angle * a},
+        {-cos_angle * b, sin_angle * b},
+        {-b, 0},
+        {0, a},
+        {-sin_angle * a, cos_angle * a},
+    };
+
+    // Push the initial point to the corners
+    corners.push_back(Eigen::Vector2d(0.0, 0.0));
+
+    // Calculate corner points based on the direction vectors
+    for (const auto& [dx, dy] : direction_vectors) {
+        Eigen::Vector2d corner_point = {corners.back().position[0] + dx, corners.back().position[1] + dy};
+        corners.push_back(Eigen::Vector2d(corner_point[0], corner_point[1]));
+    }
+
+    for (auto& corner : corners) {
+        if (std::abs(corner.position[0]) < 1e-5) corner.position[0] = 0.0;
+        if (std::abs(corner.position[1]) < 1e-5) corner.position[1] = 0.0;
+    }
 }
