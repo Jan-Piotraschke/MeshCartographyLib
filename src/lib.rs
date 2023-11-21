2c12d1d429f74f7ee03097fcb550d1758d655ded
Jan-Piotraschke <piotraschke.janniklas@gmail.com>
2023-11-21 15:43:48 +0100

    forming the square boundary

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,6 @@ use std::io::{Write, Result};
 use std::path::PathBuf;
 use std::collections::HashMap;
 
-// use ffi::ToCppString;
 extern crate tobj;
 extern crate tri_mesh;
 
@@ -18,7 +17,8 @@ use tri_mesh::Mesh;
 
 #[wasm_bindgen]
 pub fn read_mesh_from_file() {
-    println!("Reading mesh from file...");
+    log::info!("Reading mesh from file...");
+
     let mesh_cartography_lib_dir_str = env::var("Meshes_Dir").expect("MeshCartographyLib_DIR not set");
     let mesh_cartography_lib_dir = PathBuf::from(mesh_cartography_lib_dir_str);
     let new_path = mesh_cartography_lib_dir.join("ellipsoid_x4_open.obj");
@@ -29,20 +29,79 @@ pub fn read_mesh_from_file() {
 
     // Test if the mesh is valid
     // println!("{:?}", surface_mesh.is_valid());
-    println!("Mesh is closed: {:?}", surface_mesh.is_closed());
-
-    // Test if the mesh was created correctly
-    // assert_eq!(surface_mesh.no_vertices(), 4725);
-    // assert_eq!(surface_mesh.no_faces(), 9336);
+    assert!(!surface_mesh.is_closed(), "Mesh is not open");
 
     // Save the mesh to a file
     let save_path = mesh_cartography_lib_dir.join("ellipsoid_x4_edited.obj");
     save_mesh_as_obj(&surface_mesh, save_path.clone()).expect("Failed to save mesh to file");
 
     // Find the boundary vertices
-    let boundary_vertices = find_boundary_vertices(&surface_mesh);
-    // print the size
-    println!("Boundary vertices: {:?}", boundary_vertices.len());
+    find_boundary_vertices(&surface_mesh);
+}
+
+
+pub fn find_boundary_vertices(surface_mesh: &Mesh) -> Vec<tri_mesh::VertexID> {
+    let mut boundary_edges = Vec::new(); // A vector to store boundary edges
+    let mut length = 0.0;
+    let mut _l = 0.0;
+
+    for edge in surface_mesh.edge_iter() {
+        // Returns the vertex id of the two adjacent vertices to the given edge.
+        let v0 = surface_mesh.edge_vertices(edge).0;
+        let v1 = surface_mesh.edge_vertices(edge).1;
+
+        if surface_mesh.is_vertex_on_boundary(v0) && surface_mesh.is_vertex_on_boundary(v1) {
+            boundary_edges.push((v0, v1));
+            length += surface_mesh.edge_length(edge);
+        }
+    }
+
+    let n = boundary_edges.len();
+    println!("Length of boundary loop: {}", length);
+    // NOTE: in c++ this is the result: "Length of boundary loop: 42.3117"
+
+    // Create a map for easy look-up
+    let mut edge_map = HashMap::new();
+    for &(v0, v1) in &boundary_edges {
+        edge_map.insert(v0, v1);
+    }
+
+    // Get the first key from the HashMap
+    let start_key = *edge_map.keys().next().expect("HashMap is empty");
+
+    let mut current_key = start_key;
+    let mut boundary_vertices = Vec::new();
+
+    // Iterate through the HashMap
+    while let Some(&next_value) = edge_map.get(&current_key) {
+        boundary_vertices.push(next_value);
+        current_key = next_value;
+
+        // Break condition if the sequence becomes too long or cyclic
+        if boundary_vertices.len() > edge_map.len() {
+            break;
+        }
+    }
+
+    boundary_vertices.pop();
+    assert_eq!(boundary_vertices.len(), 112);  // Compared with result from C++
+
+    let corner_count = 4;
+    let side_length = length / corner_count as f64;
+    let step_size = length / n as f64;
+    let tolerance = 1e-4;
+
+    println!("Side length: {}", side_length);
+    println!("Step size: {}", step_size);
+    println!("Tolerance: {}", tolerance);
+    let corners = initialize_corners(side_length);
+    let tex_coords = distribute_vertices_around_square(&boundary_vertices, side_length, tolerance, length);
+
+    for tex_coord in &tex_coords {
+        println!("TexCoord: ({}, {})", tex_coord.0, tex_coord.1);
+    }
+
+    boundary_vertices
 }
 
 
@@ -73,49 +132,72 @@ fn save_mesh_as_obj(mesh: &tri_mesh::Mesh, file_path: PathBuf) -> Result<()> {
     Ok(())
 }
 
+struct TexCoord(f64, f64);
 
-pub fn find_boundary_vertices(surface_mesh: &Mesh) -> Vec<tri_mesh::VertexID> {
-    let mut boundary_edges = Vec::new(); // A vector to store boundary edges
+fn distribute_vertices_around_square(boundary_vertices: &[tri_mesh::VertexID], side_length: f64, tolerance: f64, total_length: f64) -> Vec<TexCoord> {
+    let n = boundary_vertices.len();
+    let step_size = total_length / n as f64;
+    let mut vertices = Vec::new();
 
-    for edge in surface_mesh.edge_iter() {
-        // Returns the vertex id of the two adjacent vertices to the given edge.
-        let v0 = surface_mesh.edge_vertices(edge).0;
-        let v1 = surface_mesh.edge_vertices(edge).1;
+    for i in 0..n {
+        let mut l = i as f64 * step_size;
+        let mut tex_coord;
 
-        if surface_mesh.is_vertex_on_boundary(v0) && surface_mesh.is_vertex_on_boundary(v1) {
-            boundary_edges.push((v0, v1));
+        // Determine the side and calculate the position
+        if l < side_length { // First side (bottom)
+            tex_coord = TexCoord(l / side_length, 0.0);
+        } else if l < 2.0 * side_length { // Second side (right)
+            l -= side_length;
+            tex_coord = TexCoord(1.0, l / side_length);
+        } else if l < 3.0 * side_length { // Third side (top)
+            l -= 2.0 * side_length;
+            tex_coord = TexCoord((side_length - l) / side_length, 1.0);
+        } else { // Fourth side (left)
+            l -= 3.0 * side_length;
+            tex_coord = TexCoord(0.0, (side_length - l) / side_length);
         }
-    }
 
-    // Create a map for easy look-up
-    let mut edge_map = HashMap::new();
-    for &(v0, v1) in &boundary_edges {
-        edge_map.insert(v0, v1);
-    }
-
-    // Get the first key from the HashMap
-    let start_key = *edge_map.keys().next().expect("HashMap is empty");
-
-    let mut current_key = start_key;
-    let mut boundary_vertices = Vec::new();
-
-    // Iterate through the HashMap
-    while let Some(&next_value) = edge_map.get(&current_key) {
-        boundary_vertices.push(next_value);
-        current_key = next_value;
-
-        // Optional: Break condition if the sequence becomes too long or cyclic
-        if boundary_vertices.len() > edge_map.len() {
-            break;
+        // Apply tolerance
+        if tex_coord.0 < tolerance {
+            tex_coord.0 = 0.0;
         }
+        if tex_coord.1 < tolerance {
+            tex_coord.1 = 0.0;
+        }
+
+        vertices.push(tex_coord);
     }
 
-    // ? Pop the last element from the vector
-    boundary_vertices.pop();
-
-    boundary_vertices
+    vertices
 }
 
+
+
+struct Corner {
+    position: (f64, f64),
+    side_length: f64,
+}
+
+impl Corner {
+    // Define a constructor that initializes both the position and side_length
+    fn new(position: (f64, f64), side_length: f64) -> Corner {
+        Corner {
+            position,
+            side_length,
+        }
+    }
+}
+
+fn initialize_corners(side_length: f64) -> Vec<Corner> {
+    let mut corners: Vec<Corner> = Vec::new();
+
+    corners.push(Corner::new((0.0, 0.0), side_length));
+    corners.push(Corner::new((1.0, 0.0), side_length));
+    corners.push(Corner::new((1.0, 1.0), side_length));
+    corners.push(Corner::new((0.0, 1.0), side_length));
+
+    corners
+}
 
 #[wasm_bindgen]
 extern {
