e8f8842bdef4d81a374a15ecfff0aa66e5fab7a6
Jan-Piotraschke <piotraschke.janniklas@gmail.com>
2023-11-24 09:33:04 +0100

    solving the equation system

--- src/SurfaceParametrization/harmonic_parameterization_helper.rs
+++ src/SurfaceParametrization/harmonic_parameterization_helper.rs
@@ -55,7 +55,45 @@ struct Triplet<T> {
     value: T,
 }
 use nalgebra::Vector3;
+use nalgebra::Scalar;
 use nalgebra::{Dynamic, SVD, QR, VectorN};
+use num_traits::Zero;
+use std::collections::HashMap;
+use std::ops::AddAssign;
+
+
+// Function to convert custom triplets to a CSR matrix
+fn build_csr_matrix<T: Copy + nalgebra::Scalar + Zero + AddAssign>(nrows: usize, ncols: usize, triplets: &[Triplet<T>]) -> CsrMatrix<T> {
+    let mut entries: HashMap<(usize, usize), T> = HashMap::new();
+    for triplet in triplets {
+        let key = (triplet.row, triplet.col);
+        *entries.entry(key).or_insert_with(Zero::zero) += triplet.value;
+    }
+
+    // Sort entries: first by row, then by column
+    let mut sorted_entries: Vec<_> = entries.into_iter().collect();
+    sorted_entries.sort_by_key(|&((row, col), _)| (row, col));
+
+    // Convert the sorted entries to vectors for CSR matrix construction
+    let mut values = Vec::new();
+    let mut row_indices = Vec::new();
+    let mut col_ptrs = vec![0; nrows + 1];
+
+    for ((row, col), value) in sorted_entries {
+        values.push(value);
+        row_indices.push(col);  // Note: col indices for each row
+        col_ptrs[row + 1] += 1;
+    }
+
+    // Compute the starting index of each row
+    for i in 1..=nrows {
+        col_ptrs[i] += col_ptrs[i - 1];
+    }
+
+    // Create the CSR matrix
+    CsrMatrix::try_from_csr_data(nrows, ncols, col_ptrs, row_indices, values)
+        .expect("Failed to create CSR matrix")
+}
 
 
 fn cholesky_solve(L: &CsrMatrix<f64>, B: &DMatrix<f64>, is_constrained: impl Fn(usize) -> bool) -> Result<DMatrix<f64>, String> {
@@ -104,28 +142,20 @@ fn cholesky_solve(L: &CsrMatrix<f64>, B: &DMatrix<f64>, is_constrained: impl Fn(
         }
     }
 
-    // Convert L to a dense matrix
-    let mut dense_L = DMatrix::zeros(ncols, ncols);
-    for triplet in L.triplet_iter() {
+    let csr_matrix = build_csr_matrix(n_dofs, n_dofs, &triplets);
+
+    // Convert CSR matrix to dense matrix
+    let mut dense_matrix = DMatrix::zeros(csr_matrix.nrows(), csr_matrix.ncols());
+    for triplet in csr_matrix.triplet_iter() {
         let i = triplet.0;
         let j = triplet.1;
-        let v = *triplet.2; // Dereference the value
+        let v = *triplet.2;
 
-        dense_L[(i, j)] = v;
+        dense_matrix[(i, j)] = v;
     }
-    // n_dofs: 4613
-    println!("n_dofs: {:?}", n_dofs);
-    println!("size of triplets: {:?}", triplets.len());
 
-    // print size of BB and dense_L
-    println!("BB: {:?} {:?}", BB.nrows(), BB.ncols());
-    println!("dense_L: {:?} {:?}", dense_L.nrows(), dense_L.ncols());
-//     std::cout << BB.rows() << " " << BB.cols() << std::endl;
-//     std::cout << AA.rows() << " " << AA.cols() << std::endl;
-//     4613 2
-// 4613 4613
     // Perform QR decomposition
-    let qr = QR::new(dense_L.clone());
+    let qr = QR::new(dense_matrix.clone());
 
     // Solve the system Lxx = BB using QR decomposition
     let xx = qr.solve(&BB)
@@ -139,57 +169,4 @@ fn cholesky_solve(L: &CsrMatrix<f64>, B: &DMatrix<f64>, is_constrained: impl Fn(
     }
 
     Ok(X)
-
 }
-
-// // Define your matrix 'A'
-// let a: DMatrix<f64> = DMatrix::identity(4, 4); // Replace with your matrix
-
-// // Define a known vector 'b' using `from_vec`
-// let b = VectorN::<f64, Dynamic>::from_vec(vec![1.0, 2.0, 3.0, 4.0]); // Replace with your vector
-
-// // Perform SVD
-// let svd = SVD::new(a, true, true);
-
-// // Solve Ax = b using SVD
-// let x = svd.solve(&b, 1e-12).expect("Cannot solve system");
-
-// println!("Solution x:\n{}", x);
-
-
-// DenseMatrix cholesky_solve(const SparseMatrix& A, const DenseMatrix& B,
-//                            std::function<bool(unsigned int)> is_constrained,
-//                            const DenseMatrix& C)
-// {
-
-
-
-
-//     SparseMatrix AA(n, n);
-//     AA.setFromTriplets(triplets.begin(), triplets.end());
-
-//     // factorize system
-//     Eigen::SimplicialLDLT<SparseMatrix> solver;
-//     solver.compute(AA);
-//     if (solver.info() != Eigen::Success)
-//     {
-//         auto what =
-//             std::string{__func__} + ": Failed to factorize linear system.";
-//         throw SolverException(what);
-//     }
-
-//     // solve system
-//     const DenseMatrix XX = solver.solve(BB);
-//     if (solver.info() != Eigen::Success)
-//     {
-//         auto what = std::string{__func__} + ": Failed to solve linear system.";
-//         throw SolverException(what);
-//     }
-
-//     // build full-size result vector from solver result (X) and constraints (C)
-//     DenseMatrix X(B.rows(), B.cols());
-//     for (int i = 0; i < A.cols(); ++i)
-//         X.row(i) = idx[i] == -1 ? C.row(i) : XX.row(idx[i]);
-
-//     return X;
-// }
